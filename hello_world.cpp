f56b9dc1dbc7932abaa9881e6f18db89f5a61344
MxVndI <kingknife29@gmail.com>
2024-11-12 11:55:15 +0300

    change comments

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-using namespace std;
 
 int main()
 {
@@ -8,5 +7,9 @@ int main()
   string name;
   cin >> name;
   cout << "Hello World from " << name;
+
+//gfdgfdgfdgfdg
+//ahahha
+// gfdhfdgfdafasfs
   return 0;
 }
